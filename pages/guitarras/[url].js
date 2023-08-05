43d13f0cefc99208a3e0e981258ebc0e470e5000
DavidAraya120312 <davidarayafernand@gmail.com>
2023-08-04 22:05:04 -0400

    commit
    actualizacion

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,19 +1,17 @@
-import Image from "next/future/image"
-import styles from '../../styles/guitarras.module.css'
-import Layout from "../../components/layout"
-import guitarrasData from '../../components/response.json'
+import Image from "next/future/image";
+import styles from '../../styles/guitarras.module.css';
+import Layout from "../../components/layout";
+import guitarrasData from '../../components/response.json';
 
-export default function Producto({guitarra}) {
+export default function Producto({ guitarra }) {
 
-    const { nombre, descripcion, imagen, precio } = guitarra[0].attributes
+    // Remover [0], ya que estás enviando solo una guitarra específica
+    const { nombre, descripcion, imagen, precio } = guitarra.attributes;
 
     return (
-        <Layout
-            title={`Guitarra ${nombre}`}
-        >
+        <Layout title={`Guitarra ${nombre}`}>
             <div className={styles.guitarra}>
                 <Image src={imagen} width={600} height={400} alt={`Imagen guitarra ${nombre}`} />
-
                 <div className={styles.contenido}>
                     <h3>{nombre}</h3>
                     <p className={styles.descripcion}>{descripcion}</p>
@@ -21,30 +19,32 @@ export default function Producto({guitarra}) {
                 </div>
             </div>
         </Layout>
-    )
+    );
 }
 
 export async function getStaticPaths() {
     const guitarras = guitarrasData.data;
 
-    const { data } = guitarras
+    const paths = guitarras.map(guitarra => {
+        console.log("URL:", guitarra.attributes.url);
+        return {
+            params: {
+                url: guitarra.attributes.url
+            }
+        };
+    })
 
-    const paths = data.map(guitarra => ({
-        params: {
-            url: guitarra.attributes.url
-        }
-    }))
     return {
-        paths, 
+        paths,
         fallback: false
     }
 }
 
-export async function getStaticProps({params: { url }}) {
-    const guitarras = guitarrasData.data;
-  return {
-    props: {
-      guitarras
-    }
-  }
+export async function getStaticProps({ params: { url } }) {
+    const guitarra = guitarrasData.data.find(g => g.attributes.url === url); // Usar find() en lugar de filter() para obtener un único objeto
+    return {
+        props: {
+            guitarra  // Enviar solo la guitarra que coincide con la URL
+        }
+    };
 }
